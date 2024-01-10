71eab2d148fb4eccdef3142b18d02f5ea0944f72
Douglas Wahast da Costa <douglaswahastdacosta@MacBook-Air-de-Douglas.local>
2024-01-10 01:27:38 -0300

    Nav and SideBar

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'compressor'
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,9 @@ STATIC_ROOT = BASE_DIR.parent / "local-cdn" / "static"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+COMPRESS_ROOT = BASE_DIR / 'static'
+
+COMPRESS_ENABLED = True
+
+STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',)
