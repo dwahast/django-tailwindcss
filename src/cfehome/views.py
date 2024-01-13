45c73e89e57feb4ba7239802e80dbe566a75fb14
Douglas Wahast da Costa <douglaswahastdacosta@MacBook-Air-de-Douglas.local>
2024-01-13 12:49:12 -0300

    Organizing

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -3,13 +3,23 @@ from django.shortcuts import render
 
 def home_view(request):
     items = [
-        {"title": "Faturas"},
-        {"title": "Saldos e Limites"},
-        {"title": "Cobrança", "has_child": True, "childs": [
-            {"title": "Inadimplência"},
-            {"title": "Pagamentos"}
-            ]},
-        {"title": "Campanhas"},
+        {
+            "title": "Faturas"
+        },
+        {
+            "title": "Saldos e Limites"
+        },
+        {
+            "title": "Cobrança",
+            "has_child": True,
+            "childs": [
+                {"title": "Inadimplência", "link": "/defaulter"},
+                {"title": "Pagamentos", "link": "/payment"}
+            ]
+        },
+        {
+            "title": "Campanhas",
+        },
     ]
     
     html_markdown = {
