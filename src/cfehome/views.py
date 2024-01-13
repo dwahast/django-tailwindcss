9ac4a378a823be8ea2179697d49bece1c7db6526
Douglas Wahast da Costa <douglaswahastdacosta@MacBook-Air-de-Douglas.local>
2024-01-13 10:26:36 -0300

    Organizing Dirs

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -2,4 +2,14 @@ from django.shortcuts import render
 
 
 def home_view(request):
-    return render(request, "pages/home.html", {})
+    items = [
+        {"title": "Faturas"},
+        {"title": "Saldos e Limites"},
+        {"title": "Cobrança", "has_child": True, "childs": [
+            {"title": "Inadimplência"},
+            {"title": "Pagamentos"}
+            ]},
+        {"title": "Campanhas"},
+    ]
+    context = {"items": items}
+    return render(request, "pages/home.html", context)
